cf3d4275e5968a2c3fb3ca0f52b7aec1281ff19e
Skill Template <template@skill>
2026-05-06 09:51:55 +0800

    Email password only by Mateo

--- api/router.ts
+++ api/router.ts
@@ -1,6 +1,5 @@
 import { authRouter } from "./auth-router";
 import { localAuthRouter } from "./local-auth-router";
-import { oauthRouter } from "./oauth-router";
 import { videoRouter } from "./video-router";
 import { createRouter, publicQuery } from "./middleware";
 
@@ -8,7 +7,6 @@ export const appRouter = createRouter({
   ping: publicQuery.query(() => ({ ok: true, ts: Date.now() })),
   auth: authRouter,
   localAuth: localAuthRouter,
-  oauth: oauthRouter,
   video: videoRouter,
 });
 
